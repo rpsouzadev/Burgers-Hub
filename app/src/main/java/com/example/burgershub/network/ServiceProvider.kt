fde6203ec0cc3ff3fe70d9cde0045a21e28a7125
Rafael Souza <rpsouza.dev@gmail.com>
2024-05-18 12:21:03 -0300

    Add OkHttp configuration and Retrofit configuration

--- app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
+++ app/src/main/java/com/example/burgershub/network/ServiceProvider.kt
@@ -1,5 +1,25 @@
 package com.example.burgershub.network
 
-class ServiceProvider {
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import java.util.concurrent.TimeUnit
 
+
+class ServiceProvider {
+  private val baseUrl = ""
+
+  private val client = OkHttpClient.Builder()
+    .connectTimeout(30, TimeUnit.SECONDS)
+    .readTimeout(30, TimeUnit.SECONDS)
+    .build()
+
+  private val retrofit: Retrofit = Retrofit.Builder()
+    .baseUrl(baseUrl)
+    .addConverterFactory(GsonConverterFactory.create())
+    .client(client)
+    .build()
+
+  fun <API> createService(apiClass: Class<API>): API = retrofit.create(apiClass)
 }
